5e7772009e9c57bfb8a466c5bf68d3277a562f8f
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-15 15:25:38 +0800

    update 9.15.15.25

---
+++ Alpha/Analysis_Mould.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'Team19029042'
+__data__ = '2017/9/15'
+
+
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_excel('../A/A题附件.xls', sheetname=1, header=None).values
+# Get the data from attachment
+for i in range(180):
+    print(i, np.min(data[:, i]))
